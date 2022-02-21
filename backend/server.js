3a8d481b782d516f3964ad70205b3b77dac15a64
padra1386 <padrahani@gmail.com>
2022-02-21 22:02:34 +0330

    add and delete

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,34 @@
 const express = require("express");
-const cors = require("cors");
+var cors = require("cors");
 const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDB = require("./config/db");
+const allowCrossDomain = require("./middleware/corsMiddleware");
 
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+
 const port = process.env.PORT;
 
 connectDB();
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(cors());
 
 app.use(errorHandler);
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [goals, setGoals] = useState([]);
+  const [showGoals, setShowGoals] = useState(true);
+  const [goalExist, setGoalExist] = useState(false);
+  const [deleted, setDeleted] = useState(false);
+  const [added, setAdded] = useState(false);
+
+  const goalInput = useRef(null);
+
+  let jsonHandler = (data) => {
+    let todos = Object.entries(data).map(([key, value]) => {
+      return {
+        ...value,
+        key,
+      };
+    });
+
+    setGoals(todos);
+  };
+
+  const postGoal = (e, inputRef) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/goals", { text: inputRef })
+      .then((res) => setAdded(true));
+
+    setAdded(false);
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/goals")
+      .then((res) => {
+        jsonHandler(res.data);
+        if (res.data) {
+          setGoalExist(true);
+        } else {
+          setGoalExist(false);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [added, deleted]);
+
+  const deleteGoal = (e, goalId) => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/api/goals/${goalId}`)
+      .then((res) => {
+        setDeleted(true);
+      })
+      .catch((err) => console.log(err));
+    setDeleted(false);
+  };
+
+  const styles = {
+    button:
+      "ring-[3px] ml-7 ring-blue-500 px-12 text-blue-100 hover:bg-blue-900 bg-blue-800 py-3 rounded-full",
+    deleteButton:
+      "ring-[2px] ml-7 ring-red-500 px-8 text-red-100 hover:bg-red-900 bg-red-800 py-2 rounded-full",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="min-h-screen bg-gray-900">
+      <div className="App">
+        <div className="postDiv pb-10">
+          <h1 className="text-7xl pt-20 mb-10 text-white text-blue-600">
+            Add a Goal
+          </h1>
+          <input
+            className="rounded-2xl px-4 py-2 w-1/4 bg-blue-900 text-gray-100 placeholder:text-gray-300 border-none  focus:ring-[1.5px] focus:outline-none focus:border-none focus:ring-blue-500 "
+            type="text"
+            ref={goalInput}
+            placeholder="Write Goal..."
+          />
+          <button
+            onClick={(e) => {
+              postGoal(e, goalInput.current.value);
+            }}
+            className={styles.button}
+          >
+            Add
+          </button>
+        </div>
+        <h2 className="mt-6 mb-12 text-blue-400 text-5xl">See your Goals</h2>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setShowGoals(true);
+          }}
+          className={styles.button}
         >
-          Learn React
-        </a>
-      </header>
+          Show 👓
+        </button>
+
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setShowGoals(false);
+          }}
+          className={styles.button}
+        >
+          Hide 🙈{" "}
+        </button>
+
+        {showGoals && goalExist ? (
+          goals.map((goal) => {
+            return (
+              <div key={goal.key}>
+                <h1 className="text-blue-400 text-[1.5rem] font-semibold mb-6 mt-7 flex flex-col justify-center items-center">
+                  {goal.text}
+                </h1>
+                <button
+                  onClick={(e) => deleteGoal(e, goal._id)}
+                  className={styles.deleteButton}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })
+        ) : (
+          <h2 className="text-blue-400 text-4xl font-semibold mt-14 flex flex-col justify-center items-center">
+            No Goals
+          </h2>
+        )}
+      </div>
     </div>
   );
 }
